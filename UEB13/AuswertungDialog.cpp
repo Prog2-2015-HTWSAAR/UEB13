3de15930b7d4c914d435570b3f8ff59a0fa37f2a
As1259 <Schreinerandreas1@gmx.net>
2015-07-25 00:18:33 +0200

    12312

---
+++ UEB13/AuswertungDialog.cpp
@@ -0,0 +1,39 @@
+#include "AuswertungDialog.h"
+const int AuswertungDialog::ZERO_VALUE = 0;
+const int AuswertungDialog::HIGH_VALUE = 1000;
+const char* AuswertungDialog::error_std = "Datei existiert nicht!!";
+
+AuswertungDialog::AuswertungDialog(){}
+AuswertungDialog::~AuswertungDialog(){}
+
+void AuswertungDialog::startDialog(){
+	do{} while (!readFileName());
+}
+
+bool AuswertungDialog::readFileName(){
+	eingabe = readStringInput();
+	if (fileExists(eingabe)){
+		fileExist = true;
+		auswert.initAuswertung();
+	}
+	else {
+		fileExist = false;
+		cout << error_std;
+	}
+
+}
+string AuswertungDialog::readStringInput() {
+	string input;
+	cin >> input;
+	clearInput();
+	return input;
+}
+void AuswertungDialog::clearInput() {
+	cin.clear();
+	cin.ignore(HIGH_VALUE, '\n');
+}
+bool AuswertungDialog::fileExists(string fileName) {
+	const char* constName = fileName.c_str();
+	ifstream infile(constName);
+	return infile.good();
+}
---
+++ UEB13/AuswertungDialog.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef AUSWERTUNGDIALOG_H_
+#define AUSWERTUNGDIALOG_H_
+#include <iostream>
+#include <string>
+#include <sstream>    
+#include <fstream>
+#include "Auswertung.h"
+using namespace std;
+
+
+class AuswertungDialog
+{
+public:
+	void startDialog();
+
+	AuswertungDialog();
+	~AuswertungDialog();
+private:
+	bool readFileName();
+	static const char* error_std;
+	static const int ZERO_VALUE;
+	static const int HIGH_VALUE;
+	bool fileExists(string fileName);
+	string readStringInput();
+	void clearInput();
+	string eingabe;
+	bool fileExist;
+	Auswertung auswert;
+};
+#endif
